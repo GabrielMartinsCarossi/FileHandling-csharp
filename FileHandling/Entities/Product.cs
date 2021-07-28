03ffb09b6584f164e9edc09e481fa828a8d10d5c
Gabriel Martins Carossi <gabrielmcarossi@gmail.com>
2021-07-28 15:42:36 -0300

    Implementacao do programa com leitura e gravacao em arquivo .csv

---
+++ FileHandling/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace FileHandling
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Qnt { get; set; }
+
+        public Product(string name, double price, int qnt)
+        {
+            Name = name;
+            Price = price;
+            Qnt = qnt;
+        }
+
+        public double Total()
+        {
+            return Price * Qnt;
+        }
+
+        public override string ToString()
+        {
+            return "Name,"+ Total().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ FileHandling/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace FileHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            //Reads the source file
+            Console.Write("Enter file full path: ");
+            string sourceFilePath = Console.ReadLine();
+
+            try
+            {
+                //Creates the target file in a new folder
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                string[] lines = File.ReadAllLines(sourceFilePath);
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach(string line in lines)
+                    {
+                        string[] info = line.Split(",");
+                        Product p = new Product(info[0], double.Parse(info[1], CultureInfo.InvariantCulture), int.Parse(info[2]));
+                        sw.WriteLine(p.ToString().ToUpper());
+                    }    
+                }
+
+                Console.WriteLine("File created successfully");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error has occurred: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An unexpected error has occurred! " + e.Message);
+            }
+        }
+    }
+}
